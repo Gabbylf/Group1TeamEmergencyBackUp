3346885230d71dfb7747623cf972ba9aa5a365ff
Gabbylf <gfrint@ucsc.edu>
2023-06-05 17:50:28 -0700

    welp

--- js/startingScreen.js
+++ js/startingScreen.js
@@ -7,12 +7,6 @@ button.click(function() {
     $("#graphic").empty();
     $("#graphic").append('<img src="img/eating.gif" width=650px>');
     // animation plays, then fades out, then goes to diagnosis page
-    setTimeout(function() {
-        $("#graphic").fadeOut(2000);
-    }, 1000);
-    setTimeout(function() {
-        location.href = "./02_diagnosis/index.html";
-    }, 4000);
 });
 
 // about.js - describes hepatitis A and common symptoms
@@ -172,7 +166,3 @@ function togglePopup(popupId) {
       }
     });
   });
-
-  $("#diagnosis-button").click(function(){
-    location.href = "../03_main/index.html"
-});
